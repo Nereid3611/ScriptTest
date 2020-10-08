e09937148400c3cf39105f8088c916ef69878026
Nereid3611 <71778576+Nereid3611@users.noreply.github.com>
2020-10-08 16:07:46 +0900

    Revert "Revert "Initial commit""
    This reverts commit f3f9b51e822d1f67906cadce20afd64f873c1474.

---
+++ Assets/test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] status = { 200, 53, 40, 30, 60 };
+
+            //順方向
+        Debug.Log("順方向");
+        for (int i = 0; i < status.Length; i++) 
+        { Debug.Log(status[i]); }
+
+        //逆方向
+        Debug.Log("逆方向");
+        for (int i = status.Length - 1 ; i >= 0; i--)
+        { Debug.Log(status[i]); }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
