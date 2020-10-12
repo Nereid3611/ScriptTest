9afbe894e25d466562fde1a0cbbd0b2697f0b5d7
Nereid3611 <71778576+Nereid3611@users.noreply.github.com>
2020-10-12 13:48:33 +0900

    BossMagic
    ボスクラス仕様のMAGIC関数課題

---
+++ Assets/test2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class boss 
+{
+
+    public void Attack(int Magicpoint )
+    {
+
+            Debug.Log("魔法攻撃をした。残りMPは" + Magicpoint);
+        
+    }
+
+}
+public class test2 : MonoBehaviour
+{   
+    
+    // Start is called before the first frame update
+    void Start()
+      {
+        boss Testboss = new boss ();
+        int MP = 53;
+        for (int i = MP; i > 5; i -= 5)
+        {
+            MP -= 5;
+            Testboss.Attack(MP);
+        }
+        Debug.Log("MPが足りないため魔法が使えない。");
+      }
+}
+
